9013c8ea1c9912ff777ad0848a75e59f18621cc0
MtSait <ag3996@gmail.com>
2023-11-15 23:34:45 -0300

    Propiedades Player
    Agregado:
    - Rasho laser y glow

--- juego/Naves/Player/Player.gd
+++ juego/Naves/Player/Player.gd
@@ -12,14 +12,26 @@ var dir_rotacion:int = 0
 
 ## Atributos Onready
 onready var canion:Canion = $Canion
+onready var laser:RayoLaser = $LaserBeam2D
 
 ## Métodos
+func _unhandled_input(event: InputEvent) -> void:
+	# Disparo Rayo
+	if event.is_action_pressed("disparo_secundario"):
+		laser.set_is_casting(true)
+
+	if event.is_action_released("disparo_secundario"):
+		laser.set_is_casting(false)
+	
+	
 func _integrate_forces(_state: Physics2DDirectBodyState) -> void:
 	apply_central_impulse(empuje.rotated(rotation))
 	apply_torque_impulse(dir_rotacion * potencia_rotacion)
 
+
 func _process(_delta: float) -> void:
 	player_input()
+	
 	
 ## Metodos Custom
 func player_input() -> void:
@@ -38,7 +50,8 @@ func player_input() -> void:
 	# Disparo
 	if Input.is_action_pressed("disparo_principal"):
 		canion.set_esta_disparando(true)
-		
+	
 	if Input.is_action_just_released("disparo_principal"):
 		canion.set_esta_disparando(false)
 	
+
